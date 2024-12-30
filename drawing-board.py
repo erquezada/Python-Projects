25d64e831df27935e75b04c0819ac554606a8972
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2024-12-30 16:19:02 -0700

    Create drawing-board.py

---
+++ drawing-board.py
@@ -0,0 +1,28 @@
+import turtle
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+
+def move_forward():
+    t.forward(10)
+
+def move_backward():
+    t.backward(10)
+
+def turn_left():
+    t.left(10)
+
+def turn_right():
+    t.right(10)
+
+def clear_screen():
+    t.clear()
+
+screen.listen()
+screen.onkey(move_forward, "Up")
+screen.onkey(move_backward, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear_screen, "c")
+
+turtle.done()
